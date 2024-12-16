186785e7b286ed032268a573ba240c81ac81b84e
k-akbari <khalil.akbari18@gmail.com>
2024-12-16 14:57:30 +0430

    Second Commit

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -10,25 +10,25 @@
     <p class="m-5 px-5 py-2 bg-red-300 border text-red-950">{{session('message')}}</p>
 @endif
 
-<a class="inline-block px-3 py-1 bg-sky-500 text-sky-200 hover:text-white my-3" href="{{route('product.create')}}">Create new product</a>
+<a class="inline-block px-3 py-1 text-green-700 bg-green-200 hover:bg-green-300 font-bold my-3" href="{{route('product.create')}}">Create new product</a>
 
 <table class="w-full">
     <tr>
-        <th class="p-3 border">{{__('Name')}}</th>
-        <th class="p-3 border">{{__('Description')}}</th>
-        <th class="p-3 border">{{__('Price')}}</th>
-        <th class="p-3 border">{{__('Stock')}}</th>
-        <th class="p-3 border">{{__('Photo')}}</th>
-        <th class="p-3 border">{{__('Actions')}}</th>
+        <th class="p-3 border-2 border-slate-300">{{__('Photo')}}</th>
+        <th class="p-3 border-2 border-slate-300">{{__('Name')}}</th>
+        <th class="p-3 border-2 border-slate-300">{{__('Description')}}</th>
+        <th class="p-3 border-2 border-slate-300">{{__('Price')}}</th>
+        <th class="p-3 border-2 border-slate-300">{{__('Stock')}}</th>
+        <th class="p-3 border-2 border-slate-300">{{__('Actions')}}</th>
     </tr>
 @foreach($products as $p)
     <tr class="odd:bg-white even:bg-slate-50 hover:bg-slate-200">
-        <td class="p-1 border">{{$p->name}}</td>
-        <td class="p-1 border">{{$p->description}}</td>
-        <td class="p-1 border">{{$p->price}} Afs</td>
-        <td class="p-1 border">{{$p->stock}}</td>
-        <td class="p-1 border"><img src="{{url('storage/'.$p->photo)}}" class="h-10" alt=""></td>
-        <td class="p-1 border">
+        <td class="p-1 border-2 border-slate-300"><img src="{{url('storage/'.$p->photo)}}" class="h-10" alt=""></td>
+        <td class="p-1 border-2 border-slate-300">{{$p->name}}</td>
+        <td class="p-1 border-2 border-slate-300">{{$p->description}}</td>
+        <td class="p-1 border-2 border-slate-300">$ {{$p->price}}</td>
+        <td class="p-1 border-2 border-slate-300">{{$p->stock}}</td>
+        <td class="p-1 border-2 border-slate-300">
             <a class="bg-sky-500 text-blue-200 inline-block hover:text-white px-3 py-1" href="{{route('product.edit',$p->id)}}">{{__('Edit')}}</a>
             <form class="inline-block" onsubmit="return confirm('Are you sure to delete?')" action="{{route('product.destroy',$p->id)}}" method="post">
                 @csrf

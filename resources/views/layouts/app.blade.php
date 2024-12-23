245c0098a0d1e2aaafadafb5414517396ddc13e7
k-akbari <khalil.akbari18@gmail.com>
2024-12-23 19:45:45 +0430

    Adding dir attribute to the layout tree

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{app()->getLocale()=='en'?'ltr':'rtl'}}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -47,7 +47,7 @@
                         {{ $slot }}
                     </div>
                 </div>
-                <div class="p-10 mt-10 text-center bg-slate-200 text-slate-700">&copy; copy 2024</div>
+                <div class="p-10 mt-10 text-center bg-slate-300 text-slate-700">&copy; copy 2024</div>
             </main>
         </div>
     </body>
